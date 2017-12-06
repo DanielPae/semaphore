625235fd8ac5586a5eb4c7f5d543e2c1273b9c79
DanielPae <danielpae104@gmail.com>
2017-12-06 16:35:31 -0500

    done?

--- semaphore.c
+++ semaphore.c
@@ -7,31 +7,34 @@
 #define KEY 2356891
 
 int main(int argc, char** argv){
-	if(argc > 0){
+  if(argc > 0){
 
-		if(!strcmp(argv[1], "-c")){
-			int sd = semget(KEY, 1, IPC_CREAT | IPC_EXCL);
-			if(sd >= 0){
-				if(argc > 1){
-					semctl(sd, 1, SETVAL, argv[2]);
-					printf("Semaphore created with key [%d]\nInitial value set to [%s]\n", KEY, argv[2]);
-				}
-				else printf("Semaphore created with key [%d]\nInitial value set to [0]\n", KEY);				
-			}
-			else printf("Semaphore already exists\n");
-		}	
-		else if(!strcmp(argv[1], "-v")){
-
-		}
-		else if(!strcmp(argv[1], "-r")){
-			int sd = semget(KEY, 1, 0);
-			semctl(sd, 1, IPC_RMID);
-			printf("Semaphore removed\n");
-		}
-		else printf("Not a recognized argument\n");
+    if(!strcmp(argv[1], "-c")){
+      int sd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
+      if(sd >= 0){
+	if(argc > 1){
+	  semctl(sd, 0, SETVAL, atoi(argv[2]));
+	  int val = semctl(sd, 0, GETVAL);
+	  printf("Semaphore created with key [%d]\nInitial value set to [%d]\n", KEY, val);
 	}
+	else printf("Semaphore created with key [%d]\nInitial value set to [0]\n", KEY);				
+      }
+      else printf("Semaphore already exists\n");
+    }	
+    else if(!strcmp(argv[1], "-v")){
+      int sd = semget(KEY, 0, 0644);
+      int val = semctl(sd, 0, GETVAL);
+      printf("Semaphore value [%d]\n", val);
+    }
+    else if(!strcmp(argv[1], "-r")){
+      int sd = semget(KEY, 0, 0);
+      semctl(sd, 1, IPC_RMID);
+      printf("Semaphore removed\n");
+    }
+    else printf("Not a recognized argument\n");
+  }
 	
-	else printf("Argument required\n");
+  else printf("Argument required\n");
 	
-	return 0;
+  return 0;
 }
